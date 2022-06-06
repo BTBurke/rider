42ca96fe31e2b9f2054c73dfb291e0c637721f58
Bryan Burke <bryan.t.burke@gmail.com>
2022-06-06 21:51:13 +0300

    slightly larger text for location data on big screens

--- src/routes/track/[user].svelte
+++ src/routes/track/[user].svelte
@@ -92,7 +92,7 @@ const refresh = async () => {
     {#if $liveTrack?.last_position.display}
         <div class="mt-4 z-50 absolute t-0 w-screen">
             <div class="p-0 bg-white flex flex-row flex-space-between max-w-md md:max-w-md w-auto rounded-lg mr-2 ml-2 md:m-auto">
-                    <div class="flex-grow m-auto ml-4 lg:text-lg">{updating ? "Updating..." : $liveTrack.last_position.display}</div>
+                    <div class="flex-grow m-auto ml-4 text-md lg:text-lg">{updating ? "Updating..." : $liveTrack.last_position.display}</div>
                     <div class="mr-1 m-auto" on:click={refresh}>
                         <button class="w-12 h-12 text-2xl" on:click={refresh} disabled={updating}>
                             <WiRefresh/>
